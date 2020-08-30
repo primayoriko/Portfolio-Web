42f03a27b9f3189526be12e612cb1fbfee8611e8
Naufal Prima Yoriko <primayoriko@gmail.com>
2020-08-30 20:00:58 +0700

    add navbar

--- static/js/index.js
+++ static/js/index.js
@@ -6,14 +6,16 @@ function getStyle(id, name)
                 window.getComputedStyle(element, null).getPropertyValue(name) : null;
 }
 
-function toggleContent(sectionId = ""){ 
+function toggleContent(arrow, sectionId = ""){ 
     var elmt = document.getElementById(sectionId);
     var display = getStyle(sectionId, "display");
     if(elmt != null){
         if(display === "none"){
             elmt.style.display = "block";
+            arrow.className = "arrow up";
         } else {
             elmt.style.display = "none";
+            arrow.className = "arrow down";
         }
     }
 }
